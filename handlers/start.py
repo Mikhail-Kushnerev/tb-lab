664969df27bf7cc08229d5528f7e5bbb51488163
mikhail <mikushnnerev@yandex.ru>
2022-08-19 02:24:36 +0300

    tg can change msg + give more info

--- handlers/start.py
+++ handlers/start.py
@@ -1,4 +1,6 @@
-import re
+import time
+from aiogram import types
+from aiogram.utils import markdown
 
 from config import dp, BASE_DIR, DOWNLOAD_DIR
 from pyppeteer import launch
@@ -21,19 +23,25 @@ async def search(message):
     except Exception:
         print("q")
     else:
-        msg = await message.answer("Запрос принят")
+        msg = await message.reply_photo(
+            photo="https://sun9-85.userapi.com/impg/qfz8AxGHPJt-oMfwAnvM17LlvaRK9jIhC3FAvQ/vexkCYmaXY4.jpg?size=676x694&quality=96&sign=0128240c61865b40f3f4bcafcec9a5c9&type=album",
+            caption="Запрос принят",
+            reply=False
+        )
         target = await check_connect(url)
         if not target:
             return
+        start = time.perf_counter()
         browser = await launch()
         page = await browser.newPage()
         await page.goto(
             url,
             {"waitUntil": "networkidle2"}
         )
+        path = f"{DOWNLOAD_DIR}\google.png"
         await page.screenshot(
             {
-                "path": f"{DOWNLOAD_DIR}\google.png",
+                "path": path,
             }
         )
         element = await page.querySelector('title')
@@ -41,13 +49,20 @@ async def search(message):
         text = "\n".join(
             (
                 title,
-                url,
-                "time"
+                f"\n{markdown.hbold('Веб - сайт')}: {url}\n",
+                "Время обработки: {} секунд"
             )
         )
-        await dp.bot.edit_message_text(
+        with open(path, 'rb') as file:
+            await dp.bot.edit_message_media(
+                chat_id=message.from_user.id,
+                message_id=msg.message_id,
+                media=types.InputMediaPhoto(file)
+            )
+        await dp.bot.edit_message_caption(
             chat_id=message.from_user.id,
             message_id=msg.message_id,
-            text=text
+            caption=text.format(int(time.perf_counter() - start)),
+            parse_mode='html'
         )
         await browser.close()
